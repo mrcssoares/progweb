09194fbb86f8ccaa13c4d481eccc36c8d36dc46c
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2016-09-16 14:02:45 -0400

    correção de bugs

--- PHPOO/EquipamentoSonoro.php
+++ PHPOO/EquipamentoSonoro.php
@@ -1,1 +1,26 @@
-EquipamentoSonoro.php
+<?php
+namespace Equipamento\Sonoro;
+
+class EquipamentoSonoro extends Equipamento{
+	private $volume;
+	private $stereo;
+
+	public mono(){
+		$this->$stereo = false;
+	}
+
+	public stereo(){
+		$this->$stereo = true;
+	}
+	public setVolume($valor){
+		if( ($valor>0) && ($valor<10) ){
+			$volume = $valor;
+		}
+	}
+	public liga(){
+		parent::liga();
+		$volume = 5;
+	}
+}
+
+?>
